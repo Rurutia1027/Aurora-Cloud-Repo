4c9df3147972441a84889c0f2a51ed2db7948bbf
Rurutia1027 <rurutia1027@gmail.com>
2025-12-07 13:11:17 +0800

    fix migration created table mismatch error

--- notification/src/main/resources/db/migration/V1__create_notification_table.sql
+++ notification/src/main/resources/db/migration/V1__create_notification_table.sql
@@ -1,7 +1,10 @@
-CREATE TABLE IF NOT EXISTS notifications
+
+CREATE TABLE IF NOT EXISTS notification
 (
-    id          BIGSERIAL PRIMARY KEY,
-    customer_id BIGINT NOT NULL,
-    message     TEXT   NOT NULL,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    notification_id      INTEGER PRIMARY KEY DEFAULT nextval('notification_id_sequence'),
+    to_customer_id       INTEGER NOT NULL,
+    to_customer_email    VARCHAR(255) NOT NULL,
+    sender               VARCHAR(255) NOT NULL,
+    message              TEXT NOT NULL,
+    sent_at              TIMESTAMP NOT NULL
 );
