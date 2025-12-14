c2e45e18d6fc5c264ef7fb5db7ee29a22d66c196
Rurutia1027 <rurutia1027@gmail.com>
2025-12-14 17:42:44 +0800

    refine docker build logic in both ci pipeline and shell script

--- bin/build-images-dockerhub.sh
+++ bin/build-images-dockerhub.sh
@@ -5,24 +5,25 @@ set -e
 TAG=$1
 LATEST_TAG="latest"
 
+if [ -z "$TAG" ]; then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
 modules=(customer fraud notification apigw)
 
-# Build and tag images
+# Build and push multi-arch images
 for module in "${modules[@]}"; do
   echo "Building Docker image for $module ..."
-
-  # Build with unique tag
-  docker build --platform  linux/amd64,linux/arm64 -t nanachi1027/aurora-$module:$TAG ./$module
-
-  # Tag also as latest
-  docker tag nanachi1027/aurora-$module:$TAG nanachi1027/aurora-$module:$LATEST_TAG
+  docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    -t nanachi1027/aurora-$module:$TAG \
+    -t nanachi1027/aurora-$module:$LATEST_TAG \
+    --push \
+    ./$module
 done
 
-# List images
-docker images | grep aurora | grep nanachi1027
-
-# Push both tags
+# Inspect pushed images (optional)
 for module in "${modules[@]}"; do
-  docker push nanachi1027/aurora-$module:$TAG
-  docker push nanachi1027/aurora-$module:$LATEST_TAG
+  docker buildx imagetools inspect nanachi1027/aurora-$module:$TAG
 done
