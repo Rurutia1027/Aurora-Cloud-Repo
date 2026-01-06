c8cb523a66e2b85fcbf7ef0e2c4b0fd54306e34c
Rurutia1027 <rurutia1027@gmail.com>
2026-01-06 19:50:23 +0800

    add module fraud source code and spring local and prod yaml files

---
+++ fraud/src/main/java/com/aurora/fraud/service/FraudCheckService.java
@@ -0,0 +1,27 @@
+package com.aurora.fraud.service;
+
+import com.aurora.fraud.dto.FraudCheckHistory;
+import com.aurora.fraud.repo.FraudCheckHistoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Integer customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return false;
+    }
+
+}
